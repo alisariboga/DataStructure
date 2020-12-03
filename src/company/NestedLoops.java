6ca1a5e46098401f4fdb8095e154e598d5ea6731
alsrbga <alican.sariboga.acs@gmail.com>
2020-12-03 18:02:46 +0000

    Some Fixed

--- src/company/NestedLoops.java
+++ src/company/NestedLoops.java
@@ -2,6 +2,14 @@ package company;
 
 public class NestedLoops {
     public static void main(String[] args) {
+        for (int i = 1; i < 20; i+=2){
+            for (int j = 0; j < i; j++){
 
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
     }
+    // yazdirdigimiz ucgeni tamamla!
 }
